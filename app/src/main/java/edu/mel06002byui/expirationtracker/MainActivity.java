5307845454787c7f679f4e8918815a9c4b0f0af5
Roman-Sanchez <romanfsanchez@gmail.com>
2015-02-18 08:32:56 -0700

    added a simple comment

--- app/src/main/java/edu/mel06002byui/expirationtracker/MainActivity.java
+++ app/src/main/java/edu/mel06002byui/expirationtracker/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
-
+    // this is a change
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
