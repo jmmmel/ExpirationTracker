77ed0210abb8bc61de00c283d9f50e248656bd74
jmmmel <youcantseeme.iamninja@gmail.com>
2015-02-25 08:34:47 -0700

    Starting test.java

---
+++ app/src/main/java/edu/mel06002byui/expirationtracker/test/ExampleTest.java
@@ -0,0 +1,10 @@
+package edu.mel06002byui.expirationtracker.test;
+
+import android.test.InstrumentationTestCase;
+
+/**
+ * Created by James on 2/25/2015.
+ */
+public class ExampleTest extends InstrumentationTestCase {
+
+}
