a30173147844326c989c8f14de27715ed9831b97
Roman-Sanchez <romanfsanchez@gmail.com>
2015-03-13 14:35:03 -0600

    changed the database constructor to take only a Context edited getGrocery with a try catch statement

--- app/src/main/java/edu/mel06002byui/expirationtracker/MainActivity.java
+++ app/src/main/java/edu/mel06002byui/expirationtracker/MainActivity.java
@@ -105,7 +105,7 @@ public class MainActivity extends ActionBarActivity {
 
     }
 
-    protected void addGroceryItem(Grocery tempGrocery){
+    protected void addGroceryItemToSet(Grocery tempGrocery){
         allStoredItems.add(tempGrocery);
     }
 
