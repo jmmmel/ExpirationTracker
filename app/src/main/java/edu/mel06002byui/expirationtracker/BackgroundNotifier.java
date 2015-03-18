5db0761065b71db3b5ac7406c1c140d651119dfc
jmmmel <youcantseeme.iamninja@gmail.com>
2015-03-18 09:53:22 -0600

    Background notification options placed in settings menu Notifications run only displays toast at this time

--- app/src/main/java/edu/mel06002byui/expirationtracker/BackgroundNotifier.java
+++ app/src/main/java/edu/mel06002byui/expirationtracker/BackgroundNotifier.java
@@ -1,36 +1,81 @@
 package edu.mel06002byui.expirationtracker;
 
-import android.app.IntentService;
 import android.app.Service;
 import android.content.Intent;
+import android.os.Handler;
 import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Created by James on 2/26/2015.
  */
-public class BackgroundNotifier extends Service{
+public class BackgroundNotifier extends Service {
+
+    public static final long NOTIFY_INTERVAL = 20 * 1000; // 20 seconds
+    private GrocerySQLiteHelper db = new GrocerySQLiteHelper(this);
+    private NotificationCompat.Builder notifyBuilder
+            = new NotificationCompat.Builder(this);
+
+    // run on another Thread to avoid crash
+    private Handler mHandler = new Handler();
+    // timer handling
+    private Timer mTimer = null;
 
     private static final String TAG = "BackgroundNotifier";
 
     @Override
-    public void onCreate(){
-        Toast.makeText(this, "Congrats! MyService Created", Toast.LENGTH_LONG).show();
-        Log.d(TAG, "onCreate");
+    public void onCreate() {
+        Toast.makeText(this, "Notificatons Started", Toast.LENGTH_LONG).show();
+        // cancel if already existed
+        if (mTimer != null) {
+            mTimer.cancel();
+        } else {
+            // recreate new
+            mTimer = new Timer();
+        }
+        // schedule task
+        mTimer.scheduleAtFixedRate(new TimeDisplayTimerTask(), 0, NOTIFY_INTERVAL);
+
     }
 
     @Override
-    public void onDestroy(){
-
+    public void onDestroy() {
+        mTimer.cancel();
+        Toast.makeText(this, "Notificatons Stopped", Toast.LENGTH_LONG).show();
     }
 
 
     @Override
     public IBinder onBind(Intent intent) {
         return null;
+    }
+
+    class TimeDisplayTimerTask extends TimerTask {
+
+        @Override
+        public void run() {
+            // run on another thread
+            mHandler.post(new Runnable() {
+
+                @Override
+                public void run() {
+                    if(db.expiringItems())
+
+                        Toast.makeText(getApplicationContext(), "Items Are Expiring",
+                            Toast.LENGTH_SHORT).show();
+                    else
+                        Toast.makeText(getApplicationContext(), "Items Are Not Expiring",
+                                Toast.LENGTH_SHORT).show();
+                }
+
+            });
+        }
     }
 }
